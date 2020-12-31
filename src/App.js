4771c1c6d4efc69f76bf35ece81450be1207bc12
Mohamed (Rafie) Elamry <mohamed.rafie@gmail.com>
2020-12-31 16:44:08 +0000

    just draw squares

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mohamed Elamry
-        </a>
-      </header>
-    </div>
-  );
+  let size = 6;
+  let rows = [];
+  for (let i = 0; i < size; i++) {
+    let cols = [];
+    for (let j = 0; j < size; j++) {
+      cols.push(
+        <div className="square col">
+          {i},{j}
+        </div>
+      );
+    }
+    rows.push(<div className="square row">{cols}</div>);
+  }
+  return <div className="App">{rows}</div>;
 }
 
 export default App;
